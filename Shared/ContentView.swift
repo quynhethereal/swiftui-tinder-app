715a5835602c3488d1b51aa648481c412a9cde6f
Quynh Le <quynh.le@employmenthero.com>
2022-09-02 11:40:23 +0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Le Helen on 02/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderAppApp.swift
+//  Shared
+//
+//  Created by Le Helen on 02/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
