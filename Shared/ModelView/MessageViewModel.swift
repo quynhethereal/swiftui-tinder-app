290a283a8de3a5f6a09fd6530e4952bd8915d451
Thuan <thanhthuan.ng92@gmail.com>
2022-09-18 15:29:19 +0700

    update header

--- Shared/ModelView/MessageViewModel.swift
+++ Shared/ModelView/MessageViewModel.swift
@@ -1,9 +1,17 @@
-//
-//  MessageViewModel.swift
-//  TinderApp (iOS)
-//
-//  Created by Le Helen on 18/09/2022.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 3
+  Author: Error Team
+     Duong Tuan Dat - s3636739
+     Le Trung Kim - s3634824
+     Le Dinh Ngoc Quynh - s3791159
+     Thuan Nguyen - s3517236
+  Created  date: 27/08/2022
+  Last modified: 18/09/2022
+  Acknowledgement: Acknowledge the resources that you use here.
+*/
 
 import Foundation
 import Firebase
--- Shared/View/MainViewGroup/ChatViewGroup/BotResponse.swift
+++ Shared/View/MainViewGroup/ChatViewGroup/BotResponse.swift
@@ -1,9 +1,17 @@
-//
-//  BotResponse.swift
-//  Tinder-FE
-//
-//  Created by Gia Huy on 12/09/2022.
-//
+/*
+  RMIT University Vietnam
+  Course: COSC2659 iOS Development
+  Semester: 2022B
+  Assessment: Assignment 3
+  Author: Error Team
+     Duong Tuan Dat - s3636739
+     Le Trung Kim - s3634824
+     Le Dinh Ngoc Quynh - s3791159
+     Thuan Nguyen - s3517236
+  Created  date: 27/08/2022
+  Last modified: 18/09/2022
+  Acknowledgement: Acknowledge the resources that you use here.
+*/
 
 import Foundation
 
