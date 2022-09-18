8456c344b5a60b0e04a442f14525f42c012aab86
dat <s3636739@rmit.edu.vn>
2022-09-18 17:50:46 +0700

    resolve conflicts

--- Shared/View/MainViewGroup/ChatViewGroup/ChatViewRow.swift
+++ Shared/View/MainViewGroup/ChatViewGroup/ChatViewRow.swift
@@ -16,91 +16,94 @@
 import SwiftUI
 
 struct ChatViewRow: View {
-    var imgName: String
-    var name: String
-    var lastMessage: String
-    var online: String
-//    @State var matcher: Matcher
+
+    //    var imgName: String
+    //    var name: String
+    //    var lastMessage: String
+    //    var online: String
+    @State var matcher: Matcher
     var body: some View {
         HStack {
-//            AsyncImage(url: URL(string: matcher.images[0]), content: view)
+            AsyncImage(url: URL(string: matcher.images[0]), content: view)
+            
+            //            Image(imgName)
+            //                .resizable()
+            //                .frame(width: 60, height: 60)
+            //                .scaledToFit()
+            //                .clipShape(Circle())
+            //                .padding(.leading)
+            //            if online == "true" {
+            //                Image(systemName: "circle.fill")
+            //                    .offset(x: -20)
+            //                    .foregroundColor(.green)
+            //            } else if online == "false" {
+            //                Image(systemName: "circle.fill")
+            //                    .offset(x: -20)
+            //                    .foregroundColor(Color("darkGrey"))
+            //            }
+            
+            VStack {
+                HStack {
+                    //                    Text(name)
+                    //                        .fontWeight(.bold)
+                    //                        .padding(.top)
+                    //                        .foregroundColor(.black)
+                    //                    Spacer()
                     
-                    Image(imgName)
-                        .resizable()
-                        .frame(width: 60, height: 60)
-                        .scaledToFit()
-                        .clipShape(Circle())
-                        .padding(.leading)
-                    if online == "true" {
-                        Image(systemName: "circle.fill")
-                            .offset(x: -20)
-                            .foregroundColor(.green)
-                    } else if online == "false" {
-                        Image(systemName: "circle.fill")
-                            .offset(x: -20)
-                            .foregroundColor(Color("darkGrey"))
-                    }
-                    
-                    VStack {
-                        HStack {
-                            Text(name)
-                                .fontWeight(.bold)
-                                .padding(.top)
-                                .foregroundColor(.black)
-                            Spacer()
-                            
-                        }
-                        HStack{
-                            Text(lastMessage)
-                                .foregroundColor(.black)
-                            Spacer()
-                        }
-                        Spacer()
-                    }
-                    .offset(x: -20)
-                    Spacer()
-//                }
-//            }
-
+                }
+                HStack{
+                    //                    Text(lastMessage)
+                    //                        .foregroundColor(.black)
+                    //                    Spacer()
+                }
+                Spacer()
+            }
+            .offset(x: -20)
+            Spacer()
         }
         .frame(height: 60)
         .padding(.vertical)
     }
-    
     @ViewBuilder
     private func view(for phase: AsyncImagePhase) -> some View {
         switch phase {
-        case .empty:
+            case .empty:
                 HStack{
                     Spacer()
                     ProgressView()
                     Spacer()
                 }
-            
-        case .success(let image):
-            image
+                
+            case .success(let image):
+                image
                     .resizable()
                     .frame(width: 60, height: 60)
                     .scaledToFit()
                     .clipShape(Circle())
                     .padding(.leading)
                 
-        case .failure(let error):
-            VStack(spacing: 16) {
-                Image(systemName: "xmark.octagon.fill")
-                    .resizable()
+            case .failure(let error):
+                VStack(spacing: 16) {
+                    Image(systemName: "xmark.octagon.fill")
+                        .resizable()
                     
                     
-                Text(error.localizedDescription)
-                    .multilineTextAlignment(.center)
-            }
-            .frame(width: 100,height: 150)
-        @unknown default:
-            Text("Unknown")
-                .foregroundColor(.gray)
+                    Text(error.localizedDescription)
+                        .multilineTextAlignment(.center)
+                }
+                .frame(width: 100,height: 150)
+            @unknown default:
+                Text("Unknown")
+                    .foregroundColor(.gray)
         }
     }
+
+    
 }
+    
+
+
+
 
 //struct ChatViewRow_Previews: PreviewProvider {
 //    static var previews: some View {
